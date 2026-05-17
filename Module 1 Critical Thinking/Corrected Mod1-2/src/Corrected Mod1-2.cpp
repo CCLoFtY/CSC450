42546f86a108005e3216958f0ca5ee97fd32df2f
Cayden Cobb <guitartok7@gmail.com>
2026-05-17 00:32:37 -0600

    Module 1 CT

---
+++ Module 1 Critical Thinking/Corrected Mod1-2/src/Corrected Mod1-2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    double myMoney = 1000.50;
+
+    // Standard Output Statement
+    cout << "Please be sure to correct all syntax errors in this program" << endl;
+
+    cout << "I have corrected all errors for this program." << endl;
+
+    cout << "The total amount of money available is = " << myMoney << endl;
+
+    // Main Function return Statement
+    return 0;
+}
---
+++ Module 1 Critical Thinking/FictionalPersonInfo/src/FictionalPersonInfo.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+   cout << "First Name: John" << endl;
+   cout << "Last Name: Smith" << endl;
+   cout << "Street Address: 123 Maple Street" << endl;
+   cout << "City: Denver" << endl;
+   cout << "Zip Code: 80202" << endl;
+   return 0;
+}
